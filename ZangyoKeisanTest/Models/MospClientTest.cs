e245fcf84956f687ea128dbf78933ff1ab163fbd
akita-n <akkie24@gmail.com>
2017-07-14 23:17:45 +0900

    Squashed 'wiki/' content from commit 0a4a836
    git-subtree-dir: wiki
    git-subtree-split: 0a4a836689e67572b323fc826963f929a85b750d

---
+++ ZangyoKeisanTest/Models/MospClientTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZangyoKeisan.Models;
+
+
+namespace ZangyoKeisanTest.Models
+{
+    class MospClientTest
+    {
+        [Test]
+        public async Task 勤怠簿ダウンロード()
+        {
+            var model = new MospClient();
+            string res = await model.downloadExcel();
+
+            Console.WriteLine(res);
+
+            Assert.That(res, Is.EqualTo("test"));
+        }
+    }
+}
